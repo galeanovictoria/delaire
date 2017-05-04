1e778dc1bfb8977c01f35cd7e53d6312a0a493a5
Victoria Galeano <galeano.victoria@gmail.com>
2017-05-05 09:16:59 +1200

    email var fix

--- resources/views/auth/emails/contact.blade.php
+++ resources/views/auth/emails/contact.blade.php
@@ -2,7 +2,7 @@
 <div style=" width=100%; background-color: #8B93A8; padding:50px; color:white;">
 
         <div>
-            <h2 style="padding-top:50px;">Thank you for contacting us, {{ $data['name'] }}!</h2>
+            <h2 style="padding-top:50px;">Thank you for contacting us!</h2>
             <h3>We will be in contact with you soon.</h3>
             <h3 style="padding-bottom:50px;">Cheers,</h3>
             <img src="{{ $message->embed(public_path() . '/assets/emaillogo.png') }}" alt="logo" width="250" />
