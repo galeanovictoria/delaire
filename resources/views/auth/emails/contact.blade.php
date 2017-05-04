40ae4b849aafee88632b6273e1b228f13728fa7c
Victoria Galeano <galeano.victoria@gmail.com>
2017-05-04 18:36:15 +1200

    email view html

--- resources/views/auth/emails/contact.blade.php
+++ resources/views/auth/emails/contact.blade.php
@@ -1,11 +1,18 @@
+<style>
+    
+    #email-contact{
+        padding: 50px;
+        color: white;
+    }
+</style>
 
-<div class="container-fluid" style="background-color: #8B93A8;">
+   <div class="container-fluid" style="background-color: #8B93A8;" id="email-contact">
     <div class="row">
         <div class="col-sm-12">
             <h2>Thank you for contacting us.</h2>
             <h3>We will be in contact with you soon!</h3>
             <h3>Cheers,</h3>
-            <img src="assets/logo.png" alt="logo" width="250">
+            <h2>Del Aire Team</h2>
         </div>
     </div>
 </div>
