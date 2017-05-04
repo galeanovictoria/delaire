05c35a23425105ce8cc2bd8c412245959a8e06eb
Victoria Galeano <galeano.victoria@gmail.com>
2017-05-05 10:48:31 +1200

    reset pass view

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,10 @@
+
+<h1 style="padding-bottom:50px;">Hi there!</h1>
+
 Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+
+
+<p style="padding-bottom:20px;">Cheers,</p>
+
+
+<a href="http://we05.galeano.victoria.yoobee.net.nz/"><img src="{{ $message->embed(public_path() . '/assets/emaillogo.png') }}" alt="logo" width="250" /></a>
