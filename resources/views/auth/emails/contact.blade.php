d309a2983e3470005e291abc9e3a1fe54e8cc351
Victoria Galeano <galeano.victoria@gmail.com>
2017-05-05 09:18:16 +1200

    email img padding fix

--- resources/views/auth/emails/contact.blade.php
+++ resources/views/auth/emails/contact.blade.php
@@ -4,7 +4,7 @@
         <div>
             <h2 style="padding-top:50px;">Thank you for contacting us!</h2>
             <h3>We will be in contact with you soon.</h3>
-            <h3 style="padding-bottom:50px;">Cheers,</h3>
+            <h3 style="padding-bottom:20px;">Cheers,</h3>
             <img src="{{ $message->embed(public_path() . '/assets/emaillogo.png') }}" alt="logo" width="250" />
         </div>
 </div>
